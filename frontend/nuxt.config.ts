720d4c6405c314f30cf2fae13a65d57e57a383b7
Dylan K <50585244+dylankainth@users.noreply.github.com>
2024-12-21 20:47:58 +0000

    tried to setup pwa

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,7 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss'
-]
+  modules: ['@nuxtjs/tailwindcss', '@vite-pwa/nuxt'],
+  pwa: {
+    manifest: {
+      name: 'My Nuxt 3 App',
+      short_name: 'NuxtApp',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff',
+      theme_color: '#ffffff',
+    },
+  },
 })
---
+++ frontend/pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <div>helloooo</div>
+
+</template>

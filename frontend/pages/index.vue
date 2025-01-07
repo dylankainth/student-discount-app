46c7d2818c59e1a8bd9daf6ffe8a0688a6b43496
Dylan K <50585244+dylankainth@users.noreply.github.com>
2025-01-07 18:44:40 +0000

    tried to shuffle around components and navbar; having nuxt errors

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,4 +1,15 @@
 <template>
-  <div>helloooo</div>
+  <div>
+
+    <div class="mb-4 items-end justify-between space-y-4 sm:flex sm:space-y-0 md:mb-8">
+
+      <AppFooter />
+    </div>
+
+
+
+
+
+  </div>
 
 </template>

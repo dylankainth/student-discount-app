dd07665145a74796161ec0e8d37dd7740cd2fe3e
Dylan K <50585244+dylankainth@users.noreply.github.com>
2025-01-17 09:42:32 +0000

    added a card to page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,33 @@
 <template>
   <div>
 
-    <div class="mb-4 items-end justify-between space-y-4 sm:flex sm:space-y-0 md:mb-8">
+    <div class="container mx-auto px-5 pt-3">
 
-      <AppFooter />
+
+      <p class="text-3xl">Welcome</p>
+      <p class="text-lg text-gray-500">Let's find you some deals.</p>
+
+
+
+      <div class="pt-5">
+        <a href="#"
+          class="flex bg-white border border-gray-200 rounded-lg shadow max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+          <img class="object-cover w-1/3 rounded-s-lg h-full" src="https://flowbite.com/docs/images/blog/image-4.jpg"
+            alt="">
+          <div class="flex flex-col justify-between p-4 leading-normal w-2/3">
+            <h5 class="mb-2 text-sm font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
+              acquisitions 2021</h5>
+            <p class="mb-3 font-normal text-xs text-gray-700 dark:text-gray-400">Here are the biggest enterprise
+              technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          </div>
+        </a>
+      </div>
+
+
+
+
+
+
     </div>
 
 
@@ -13,3 +37,7 @@
   </div>
 
 </template>
+
+<script>
+
+</script>
